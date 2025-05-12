6cc25a921761dab99169bef0582a0d98038a777f
Sergii Trotsenko <strotsenko@foundationsoft.com>
2025-05-12 14:10:53 -0400

    added bots

--- lib/clients/telegramClient.ts
+++ lib/clients/telegramClient.ts
@@ -1,6 +1,6 @@
 import fetch from "node-fetch";
 
-const sendMessage = async (message: string): Promise<any> => {
+const sendMessage = async (message: string): Promise<unknown> => {
   const url = `https://api.telegram.org/bot${process.env.TLG_TOKEN_ID}/sendMessage?chat_id=${process.env.TLG_CHAT_ID}&parse_mode=MarkdownV2`;
 
   const body = {text: escapeSpecialChars(message)};
@@ -18,23 +18,23 @@ const sendMessage = async (message: string): Promise<any> => {
 
 const escapeSpecialChars = (message: string) =>
   message
-    .replace(/\_/g, "\\_")
+    .replace(/_/g, "\\_")
     .replace(/\*/g, "\\*")
     .replace(/\[/g, "\\[")
-    .replace(/\]/g, "\\]")
+    .replace(/]/g, "\\]")
     .replace(/\(/g, "\\(")
     .replace(/\)/g, "\\)")
-    .replace(/\~/g, "\\~")
-    .replace(/\`/g, "\\`")
-    .replace(/\>/g, "\\>")
-    .replace(/\#/g, "\\#")
+    .replace(/~/g, "\\~")
+    .replace(/`/g, "\\`")
+    .replace(/>/g, "\\>")
+    .replace(/#/g, "\\#")
     .replace(/\+/g, "\\+")
-    .replace(/\-/g, "\\-")
-    .replace(/\=/g, "\\=")
+    .replace(/-/g, "\\-")
+    .replace(/=/g, "\\=")
     .replace(/\|/g, "\\|")
     .replace(/\{/g, "\\{")
-    .replace(/\}/g, "\\}")
+    .replace(/}/g, "\\}")
     .replace(/\./g, "\\.")
-    .replace(/\!/g, "\\!");
+    .replace(/!/g, "\\!");
 
 export {sendMessage};
